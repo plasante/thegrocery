25052107106c3cc3bb26622fba521ee33773ec91
Pierre Lasante <pierre.lasante@videotron.ca>
2017-06-24 13:23:54 +0000

    Add edit update show and index

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,15 @@
 class ProductsController < ApplicationController
+    
+    def index
+        @products = Product.all
+    end
+    
     def new
         @product = Product.new
+    end
+    
+    def edit
+        @product = Product.find(params[:id])
     end
     
     def create
@@ -13,6 +22,16 @@ class ProductsController < ApplicationController
         else
             # show the new product created
             render 'new'
+        end
+    end
+    
+    def update
+        @product = Product.find(params[:id])
+        if @product.update(product_params)
+           flash[:notice] = "Product was successfully updated"
+           redirect_to product_path(@product)
+        else
+            render 'edit'
         end
     end
     

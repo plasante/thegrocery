324e184f25b896ddb82063e8c8f1d5a2eba3798a
Pierre Lasante <pierre.lasante@videotron.ca>
2017-06-24 12:34:21 +0000

    Fix create, add flass and error msg.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,8 +6,18 @@ class ProductsController < ApplicationController
     def create
         #render plain: params[:product].inspect
         @product = Product.new( product_params )
-        @product.save
-        redirect_to products_show(@product)
+        if @product.save
+            # validation failed
+            flash[:notice] = "Product was successfully created"
+            redirect_to product_path(@product)
+        else
+            # show the new product created
+            render 'new'
+        end
+    end
+    
+    def show
+        @product = Product.find(params[:id])
     end
     
     private
